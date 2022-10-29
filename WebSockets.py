4b78d6cf08f25c4fe5363cf65f188b6a0c1b51a4
Juan Pablo Vargas <vargasjuan00425@gmail.com>
2022-10-29 15:40:17 -0500

    nada

--- WebSockets.py
+++ WebSockets.py
@@ -14,9 +14,16 @@ async def handler(websocket, path):
     await websocket.send(reply)
 
 
-start_server = websockets.serve(handler, "localhost", 8000)
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
-asyncio.get_event_loop().run_until_complete(start_server)
-
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    asyncio.run(main())
